80a975ecd2c0924b6be75527a823e0714b05c266
RaiyanC <raiyan.c15@gmail.com>
2022-03-11 10:54:47 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+class Test:
+    def __init__(self):
+        pass
+
+    def test(self):
+        print("test")
+
+
+if __name__ == '__main__':
+    test_class = Test()
+    test_class.test()
