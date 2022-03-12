cf40dcea250fca7b5f48d3588941599aa149a35c
Yi <rlfff1998@gmail.com>
2022-03-12 17:56:55 +0000

    EDA & Modeling (knn, lrg, gnb, dt) of delivery status & delivery risk

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,confusion_matrix,recall_score,f1_score,precision_score
 # class Wrangler:
 #     def __init__(self, directory_path):
 #         self.directory_path = directory_path
@@ -30,13 +34,15 @@ from sklearn.neighbors import KNeighborsClassifier
 
 df_order = pd.read_csv('BigSupplyCo_Data_CSVs/BigSupplyCo_Orders.csv')
 
-# overview
+# # # Explornatory Data Analysis
+# =============================================================================
+
 df_order.info()
 df_order.head()
 df_order.describe()
 
 # check na
-df_order.isnull().values.any()
+df_order.apply(lambda x: sum(x.isnull()))
 
 # delivery status
 sns.countplot(x='Delivery Status', data = df_order)
@@ -71,12 +77,54 @@ df_time = df_time.reset_index()
 df_time.rename(columns={0:'count'})
 sns.lineplot(data=df_time, x='order_month', y=0, hue = 'order_year', style='Delivery Status')
 
-# delivery status prediction
-# need to do a PCA first?
-train_data=df_order.copy()
-train_data=train_data.drop(['Order Id','Order Customer Id','Order City','Order State','Order Zipcode','order date (DateOrders)', 'Order Item Discount','Order Item Id','Sales','Order Item Total','Days for shipping (real)','Late Delivery Risk'], axis=1)
+# # # Modelling
+# =============================================================================
 
-train_data.info()
+# Classfiers
+
+knn = KNeighborsClassifier()
+lgr = LogisticRegression(solver='lbfgs', max_iter=3000)
+gnb = GaussianNB()
+dt = DecisionTreeClassifier()
+
+
+# Fidelity tests
+def fidelity_tests(model,X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 3)
+    model=Pipeline([("scaler", StandardScaler()), ("model", model)])
+    
+    model.fit(X_train,y_train) # Fitting train data for predection
+    y_pred=model.predict(X_test)
+    
+    accuracy=accuracy_score(y_test,y_pred) #Accuracy for predection 
+    recall=recall_score(y_test,y_pred,average='weighted')# Recall score for predection
+    prec=precision_score(y_test,y_pred,average='weighted')# precision score
+    conf=confusion_matrix(y_test, y_pred)#predection of late delivery
+    f1=f1_score(y_test, y_pred,average='weighted')#f1 score for prediction
+    print('Model paramters used are :',model)
+    print('Accuracy of late delivery status is:', (accuracy)*100,'%')
+    print('Recall score of late delivery status is:', (recall)*100,'%')
+    print('Precision score of late delivery status is:', (recall)*100,'%')
+    print('Conf Matrix of late delivery status is: \n',(conf))
+    print('F1 score of late delivery status is:', (f1)*100,'%')
+    
+# Cross-validation
+def cross_val(model, X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 3) 
+    model=Pipeline([("scaler", StandardScaler()), ("model", model)])
+    model.fit(X_train,y_train)# Fitting train data for predection
+    
+    cross_model = cross_val_score(estimator=model, X=X_train, y=y_train, cv=10)# 10-fold cross-validation
+
+    print("Cross validation model",cross_model)
+    print("The average accuracy of model with cross validation is:",cross_model.mean())
+
+
+train_data=df_order.copy()
+# drop the features multicoliniarity/outcome variables
+train_data=train_data.drop(['Order Id','Order Customer Id','Order City','Order State','Order Zipcode','order date (DateOrders)', 'Order Item Discount','Order Item Id','Sales','Order Item Total','Days for shipping (real)'], axis=1)
+
+# convert all object data to numerical data
 le = preprocessing.LabelEncoder()
 train_data['Market'] = le.fit_transform(train_data['Market'])
 train_data['Order Country'] = le.fit_transform(train_data['Order Country'])
@@ -85,11 +133,51 @@ train_data['Order Status'] = le.fit_transform(train_data['Order Status'])
 train_data['Type']   = le.fit_transform(train_data['Type'])
 train_data['Delivery Status'] = le.fit_transform(train_data['Delivery Status'])
 
-train_data.head()
-x=train_data.loc[:, train_data.columns != 'Delivery Status']
-y=train_data['Delivery Status']
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
-knn_pipline = Pipeline([("scaler", StandardScaler()), ("KNeighborsClassifier", KNeighborsClassifier())])
-model = knn_pipline.fit(X_train, y_train)
-knn_train_predict = pd.DataFrame({'actual' : y_train,'predicted' : model.predict(X_train)})
-knn_train_predict.head()
+
+# # # Prediction of late delivery status (0,1,2,3)
+train_data1=train_data.drop('Late Delivery Risk', axis=1)
+train_data.info()
+
+# create train/test sets
+X1=train_data.loc[:, train_data.columns != 'Delivery Status']
+y1=train_data['Delivery Status']
+
+# classfication Results
+fidelity_tests(knn,X1,y1)
+cross_val(knn,X1,y1)
+
+fidelity_tests(lgr,X1,y1)
+cross_val(lgr,X1,y1)
+
+fidelity_tests(gnb,X1,y1)
+cross_val(gnb,X1,y1)
+
+fidelity_tests(dt,X1,y1)
+cross_val(dt,X1,y1)
+
+
+
+# # # Prediction of late delivery risk (0,1)
+train_data2=train_data.drop('Delivery Status', axis=1)
+train_data.info()
+
+# create train/test sets
+X2=train_data2.loc[:, train_data2.columns != 'Late Delivery Risk']
+y2=train_data2['Late Delivery Risk']
+
+# classfication Results
+fidelity_tests(knn,X2,y2)
+cross_val(knn,X2,y2)
+
+fidelity_tests(lgr,X2,y2)
+cross_val(lgr,X2,y2)
+
+fidelity_tests(gnb,X2,y2)
+cross_val(gnb,X2,y2)
+
+fidelity_tests(dt,X2,y2)
+cross_val(dt,X2,y2)
+
+# source: https://www.kaggle.com/nilufarhosseini/supply-chain-data-analysis-99-accuracy
+# https://www.kaggle.com/pvmanish/group-9#Casestudy-2:-Late-Delivery-Classification
+# https://www.kaggle.com/skloveyyp/comparison-of-classification-regression-rnn
